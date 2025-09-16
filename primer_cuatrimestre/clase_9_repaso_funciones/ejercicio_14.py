98d79d07f69cc7c4f6c71a3b9441ca5be30f5d69
Nahuel Ruiz Diaz <rnahuellautaro@gmail.com>
2025-09-16 19:59:02 -0300

    Repaso clase 9 funciones

--- primer_cuatrimestre/clase_9_repaso_funciones/ejercicio_14.py
+++ primer_cuatrimestre/clase_9_repaso_funciones/ejercicio_14.py
@@ -1,22 +1,40 @@
-""" """
+# Crear una función que le solicite al usuario el ingreso de un número flotante y lo retorne.
 
-def validar_ingreso_numerico(ingreso:str)->str:
+# Validaciones a realizar: 
+# Que el programa no rompa al ingresar un valor no numerico
+# Que el ingreso contenga un '.' cómo máximo
+
+def validar_ingreso_flotante()-> float:
     ingreso_valido = False
-    ingreso = input("Ingrese un numero")
-    
+    ingreso = input("Ingrese un número: ")
     while not ingreso_valido:
-        ingreso_a_castear = ""
+        contador_comas = 0
+        primer_vuelta = True
         for i in range(len(ingreso)):
             match ingreso[i]:
-                case "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9":
-                    ingreso_a_castear += ingreso[i]
+                case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
                     ingreso_valido = True
+                case "-": 
+                    if not primer_vuelta:
+                        print("Ingreso invalido.")
+                        ingreso = input("Ingrese un número: ")
+                        ingreso_valido = False
+                        break
+                case ".":
+                    contador_comas += 1
+                    if contador_comas > 1:
+                        print("Ingreso invalido.")
+                        ingreso = input("Ingrese un número: ")
+                        ingreso_valido = False
+                        break
                 case _:
-                    print("Ingreso invalido")
-                    ingreso = input("Ingrese un numero: ")
+                    print("Ingreso invalido.")
+                    ingreso = input("Ingrese un número: ")
                     ingreso_valido = False
                     break
-    return int(ingreso)
+            if primer_vuelta:
+                primer_vuelta = False
+    return float(ingreso)
 
-ingreso_validado = validar_ingreso_numerico()
+ingreso_validado = validar_ingreso_flotante()
 print("Ingreso validado: ", ingreso_validado)
