116c5a486738be97e6ce81dfebcb9a269dd864fe
Nahuel Ruiz Diaz <rnahuellautaro@gmail.com>
2025-09-15 21:58:27 -0300

    clase 8 ejercicios

---
+++ primer_cuatrimestre/clase_8_repaso_funciones/ejercicio_9.py
@@ -0,0 +1,7 @@
+def tabla_de_multiplicar(numero, inicio=1, fin=1)-> None:
+    if inicio >= 1 and fin <=11
+        for i in range(inicio , fin + 1):
+            print(f"El numero {numero} X {i} ) {numero * i}")
+    else:
+        print("ERROR parametro incorrecto...")
+tabla_de_multiplicar(2,5,9)
