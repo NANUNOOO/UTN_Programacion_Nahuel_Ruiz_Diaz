cba89ff355780b9db03555e54362569db4a2fc50
Nahuel Ruiz Diaz <rnahuellautaro@gmail.com>
2025-09-23 19:15:17 -0300

    Clase 10: Listas

--- primer_cuatrimestre/clase_10_listas/ejercicio_6.py
+++ primer_cuatrimestre/clase_10_listas/ejercicio_6.py
@@ -2,9 +2,9 @@
 # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-lista2 = [None] * len(lista)
+lista2 = []
 for i in range(len(lista)):
     if lista[i] % 2 == 0:
-        lista2[i] = lista[i]
+        lista2 += [lista[i]]
 
 print(lista2)
