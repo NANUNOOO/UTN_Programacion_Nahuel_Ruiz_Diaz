4583895394230bd14411c5b8092dbcf4b330dd06
Nahuel Ruiz Diaz <rnahuellautaro@gmail.com>
2025-10-20 18:38:06 -0300

    .

---
+++ primer_cuatrimestre/clase_15_/ejercicio_1.py
@@ -0,0 +1,18 @@
+"""
+que es programar
+que es algoritmo
+lenguajes compilados o interpretado...¿que eson? ¿cual es mas rapido de los dos?
+¿que son los paradigmas? ¿que paradigma es python?
+mutabilidad/inmutabilidad
+reglas de estilo: que es PEP - PEP 8
+estructura de control -diagrama de flujo
+diferecia entre if elif
+bucle determinado indeterminado bucle while ver continue break
+inicio, fin y salto en for
+que es una funcion cuales son los parametros actuales y formales componentes de una funcion
+cohesion y acomplamiento
+que es una referencia(es una direccion de memoria)
+modulos y pquetes ¿que es un modulo? ¿que es un paquete?
+como importar paquetes (no importar con *)
+
+"""
